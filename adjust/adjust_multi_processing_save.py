bfbbed1079b2b9d4171e23273cb021215a7125b8
choiyoungmin <dudals3844@gmail.com>
2021-12-29 08:34:00 +0900

    다운로드 받고 한꺼번에 insert하는 형식으로 변경

--- adjust/adjust_multi_processing_save.py
+++ adjust/adjust_multi_processing_save.py
@@ -7,12 +7,13 @@ from adjust.models import AdjustDataModel
 from config.settings import ENGINE, ENGINE_PREPROCESSING, META
 import pandas as pd
 
-def start(ncusip: str):
+
+def insert(ncusip: str):
     try:
         conn = ENGINE_PREPROCESSING.connect()
         table = Table(AdjustDataModel.__tablename__, META, autoload_with=ENGINE_PREPROCESSING)
-        crsp = CRSP(ncusip)
-        data = crsp.get()
+        with pd.HDFStore('crsp.h5') as store:
+            data = store[ncusip]
         data = data.replace({np.nan: None})
         data = data.reset_index()
         data['ticker'] = data['ticker'].fillna(method='bfill')
@@ -23,11 +24,21 @@ def start(ncusip: str):
         print(e)
 
 
+def download(ncusip: str):
+    try:
+        crsp = CRSP(ncusip)
+        data = crsp.get()
+        with pd.HDFStore('crsp.h5') as store:
+            store.put(ncusip, data)
+
+    except Exception as e:
+        print(e)
+
 
 if __name__ == "__main__":
     sql = """SELECT DISTINCT (NCUSIP) FROM tb_names WHERE EXCHCD IN (1,2,3,4);"""
     ncusips = pd.read_sql(sql, ENGINE.connect())['NCUSIP']
-    pool = Pool(processes=1)
-    pool.map(start, ncusips)
+    pool = Pool(processes=20)
+    pool.map(download, ncusips)
     pool.close()
     pool.join()
