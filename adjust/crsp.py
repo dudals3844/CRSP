a56cd76a9f854a2693ab3dbc418f92a2d3192b59
choiyoungmin <dudals3844@gmail.com>
2021-12-29 09:25:46 +0900

    종가가 마이너스인 값들 절댓값 만드는것으로 변경

--- adjust/crsp.py
+++ adjust/crsp.py
@@ -42,7 +42,7 @@ class CRSP:
 
     def __calculate_adjust_ratio(self):
         value = self.data['profit'].shift(-1) + 1
-        adjust_price = (1 / value).iloc[::-1].cumprod().iloc[::-1] * self.data['close'].iloc[-1]
+        adjust_price = (1 / value).iloc[::-1].cumprod().iloc[::-1] * abs(self.data['close'].iloc[-1])
         adjust_price.iloc[-1] = self.data['close'].iloc[-1]
         adjust_ratio = adjust_price / self.data['close']
         return adjust_ratio
