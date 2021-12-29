bccf4fc99e89cec37ab5d7ac5d60e659c944e163
choiyoungmin <dudals3844@gmail.com>
2021-12-29 09:14:30 +0900

    바로 DB에 넣는 방식으로 변경

--- adjust/adjust_multi_processing_save.py
+++ adjust/adjust_multi_processing_save.py
@@ -12,8 +12,8 @@ def insert(ncusip: str):
     try:
         conn = ENGINE_PREPROCESSING.connect()
         table = Table(AdjustDataModel.__tablename__, META, autoload_with=ENGINE_PREPROCESSING)
-        with pd.HDFStore('crsp.h5') as store:
-            data = store[ncusip]
+        crsp = CRSP(ncusip)
+        data = crsp.get()
         data = data.replace({np.nan: None})
         data.dropna(subset=['close', 'profit'], inplace=True)
         data = data.reset_index()
@@ -21,18 +21,6 @@ def insert(ncusip: str):
         dict_data = data.to_dict('records')
         conn.execute(table.insert().prefix_with('IGNORE'), dict_data)
         del conn
-    except Exception as e:
-        print(e)
-
-
-def download(ncusip: str):
-    try:
-        crsp = CRSP(ncusip)
-        data = crsp.get()
-        data.reset_index(inplace=True)
-        with pd.HDFStore('crsp.h5') as store:
-            store.put(ncusip, data)
-
     except Exception as e:
         print(e)
 
